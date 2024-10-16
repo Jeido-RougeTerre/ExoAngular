fc7a483f8e3e1da1aa67ff5fca17adfdafd8e621
Jeido <wampawither@gmail.com>
2024-10-16 16:41:30 +0200

    ex5

--- src/app/pages/library/library.component.ts
+++ src/app/pages/library/library.component.ts
@@ -3,21 +3,41 @@ type order = 'asc' | 'desc';
 import { Component } from '@angular/core';
 import { Book } from './book';
 import { BooleanemojiPipe } from '../../utils/pipes/booleanemoji.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [BooleanemojiPipe],
+  imports: [BooleanemojiPipe, FormsModule],
   templateUrl: './library.component.html',
   styleUrl: './library.component.css'
 })
 export class LibraryComponent {
   books : Book[] = [new Book("meh.", "Me", false), new Book("awesome book", "super author", true)];
-
+  hasSubmit : boolean = false;
   order : order = "asc";
+
+  book : Book = new Book("", "", false);
 
   toggleRead(book : Book) {
     book.toggleRead();
+  }
+
+  submitBook() : void {
+    this.hasSubmit = true;
+    if(!(this.titleHasError || this.authorHasError)) {
+      this.books.push(this.book);
+      this.book = new Book("", "", false);
+      this.hasSubmit = false;
+    }
+  }
+
+  get titleHasError() {
+    return this.hasSubmit && this.book.title.length <= 0
+  }
+
+  get authorHasError() {
+    return this.hasSubmit && this.book.author.length <= 0
   }
 
 }
