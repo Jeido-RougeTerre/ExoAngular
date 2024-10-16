06f120f34ae338d4c13b7e8366373fa81a6bb071
Jeido <wampawither@gmail.com>
2024-10-16 09:52:12 +0200

    added ex2

--- exercice/src/app/pages/series/series.component.ts
+++ exercice/src/app/pages/series/series.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './series.component.css'
 })
 export class SeriesComponent {
+  series: string[] = ["item1", "item2", "item3"];
 
+  removeSerie(index : number) : boolean {
+    if (this.series.length - 1 < index || index < 0) return false;
+    this.series.splice(index, 1);
+    return true;
+  }
 }
