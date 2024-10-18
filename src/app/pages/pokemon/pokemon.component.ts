2cd10099f34708dbea41e4fea21aad5d75c32e75
Teo Gontard <teo.gontard@gmail.com>
2024-10-18 11:51:41 +0200

    aded ex7 & 8

--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,80 +1,135 @@
 import { Component } from '@angular/core';
-import { Pokemon } from './pokemon';
-import { FormArray, FormControl, FormGroup,  ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Pokemon } from '../../utils/types/pokemon.type';
+import { PokemonCardComponent } from '../../components/pokemon-card/pokemon-card.component';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, PokemonCardComponent],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.css'
 })
 export class PokemonComponent {
-  pokemons : Pokemon[] = [];
 
-  types : string[] = [
-    "NORMAL",
-    "FIRE",
-    "FIGHTING",
-    "WATER",
-    "FLYING",
-    "GRASS",
-    "POISON",
-    "ELECTRIC",
-    "GROUND",
-    "PSYCHIC",
-    "ROCK",
-    "ICE",
-    "BUG",
-    "DRAGON",
-    "GHOST",
-    "DARK",
-    "STEEL",
-    "FAIRY"
+  pokeForm = new FormGroup({
+    nom: new FormControl(""),
+    description: new FormControl(""),
+    types: new FormControl([]),
+    attacks: new FormArray([
+      new FormGroup({
+        nom: new FormControl(""),
+        description: new FormControl(""),
+        degats: new FormControl(0)
+      })
+    ]),
+    zone: new FormGroup({
+      nom: new FormControl(""),
+      region: new FormControl("")
+    })
+  })
+
+  get attacks() {
+    return this.pokeForm.controls.attacks;
+  }
+
+  addAttack() : void {
+    this.attacks.push(
+      new FormGroup({
+        nom: new FormControl(""),
+        description: new FormControl(""),
+        degats: new FormControl(0)
+      })
+    )
+  }
+
+  onSubmit(): void {
+    console.log(this.pokeForm.value);
+    this.pokeForm.reset()
+  }
+
+  types: string[] = [
+    "feu",
+    "vol",
+    "psy",
+    "poison",
+    "eau",
+    "roche",
+    "normal",
+    "plante",
+    "electrik",
+    "fee",
+    "dragon"
   ]
 
-  pokemon_form: FormGroup = new FormGroup(
+  pokemonList: Pokemon[] = [
     {
-      nom: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-      desc: new FormControl('', [Validators.required]),
-      types: new FormControl([]),
-      attacks : new FormArray([
-        new FormGroup({
-          nom: new FormControl('', [Validators.required, Validators.minLength(3)]),
-          desc: new FormControl(''),
-          dmg: new FormControl(0, [Validators.min(0)])
-        }, [Validators.required])
-      ]),
-      zone : new FormGroup({
-        nom: new FormControl('', [Validators.required]),
-        region: new FormControl("Johto", [Validators.required])
-      })
-    }, [Validators.required]);
-
-    get attacks() {
-      return this.pokemon_form.controls['attacks'] as FormArray;
-    }
-
-    addAttack() {
-      if (this.canAddAttack()) {
-        this.attacks.push(new FormGroup({
-          nom: new FormControl('', [Validators.required, Validators.minLength(3)]),
-          desc: new FormControl(''),
-          dmg: new FormControl(0, [Validators.min(0)])
-        }));
-      } else {
-        console.error("Too much attacks cannnot have more than " + Pokemon.maxAttack);
+      nom: "Dracaufeu",
+      description: "Un Pokémon volant et cracheur de feu.",
+      types: ["feu", "vol"],
+      attacks: [
+        {
+          nom: "Lance-Flammes",
+          description: "Projette des flammes intenses pour brûler l'ennemi.",
+          degats: 90
+        },
+        {
+          nom: "Danse Draco",
+          description: "Augmente l'attaque et la vitesse.",
+          degats: 0
+        }
+      ],
+      zone: {
+        nom: "Mont Braise",
+        region: "Kanto"
+      }
+    },
+    {
+      nom: "Tortank",
+      description: "Un Pokémon tortue doté de canons à eau sur son dos.",
+      types: ["eau"],
+      attacks: [
+        {
+          nom: "Hydrocanon",
+          description: "Lance un puissant jet d'eau pour submerger l'adversaire.",
+          degats: 110
+        },
+        {
+          nom: "Ébullition",
+          description: "Inflige des brûlures tout en attaquant.",
+          degats: 80
+        }
+      ],
+      zone: {
+        nom: "Lac Courage",
+        region: "Sinnoh"
+      }
+    },
+    {
+      nom: "Gardevoir",
+      description: "Un Pokémon capable de voir l'avenir et de protéger son dresseur.",
+      types: ["psy", "fee"],
+      attacks: [
+        {
+          nom: "Choc Psy",
+          description: "Inflige des dégâts en utilisant la force mentale.",
+          degats: 80
+        },
+        {
+          nom: "Pouvoir Lunaire",
+          description: "Utilise la lumière de la lune pour infliger de gros dégâts.",
+          degats: 95
+        }
+      ],
+      zone: {
+        nom: "Forêt de Jade",
+        region: "Hoenn"
       }
     }
+  ];
 
-    canAddAttack() : boolean {
-      return this.attacks.length < Pokemon.maxAttack;
-    }
-
-    submitPokemon() {
-      if (this.pokemon_form.valid){
-        console.log(this.pokemon_form.value)
-        this.pokemon_form.reset();
-      }
-    }
+  deletePokemon(pokemon : Pokemon): void {
+    const index = this.pokemonList.indexOf(pokemon)
+    this.pokemonList.splice(index, 1)
+  }
 }
--- src/app/pages/pokemon/zone.ts
+++
@@ -1,9 +0,0 @@
-export class Zone {
-    nom: string;
-    region : string;
-
-    constructor(nom : string, region : string) {
-        this.nom = nom;
-        this.region = region;
-    }
-}
